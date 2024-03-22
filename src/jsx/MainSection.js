7f63f8b3e2ccb9a4b3a503f7832c33704959b0d9
Yhumi0210 <buillaura0210@gmail.com>
2024-03-22 12:53:33 +0100

    modifs

--- src/jsx/MainSection.js
+++ src/jsx/MainSection.js
@@ -10,7 +10,7 @@ function MainSection() {
             <div className="main__container">
                 <video className="main__container__video" controls autoPlay loop src={videoSrc}></video>
             </div>
-            <h1 className="main__h1">Sublimer les projets par l’image...
+            <h1 className="main__h1">Sublimer les projets par l’image.
             </h1>
             <a href="#project-section" className="main__arrow">
                 <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
